6b328c7fa16534ef246f7f3fbdf1ebce18118cd8
Daniil Hryshayeu <grishaev.daniil1@gmail.com>
2023-12-02 23:33:40 +0200

    Added openapi doc

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of the coffee' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The brand of the coffee' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({ description: 'fl' })
   @IsString({ each: true })
   readonly flavors: string[];
 }
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import 'reflect-metadata';
 import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -21,6 +23,15 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('fundApp')
+    .setDescription('Coffee app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
